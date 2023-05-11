857dd67f1cfbb7d121d8404e2b5e996c8b717e3e
Evere77 <evere777@yahoo.com>
2023-05-10 20:36:30 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+    // create state vars
+    const [todos, setTodos] = useState([
+      {
+        todo: "Walk the dog",
+        isCompleted: false
+      },
+      {
+        todo: "Take out the trash",
+        isCompleted: false
+      },
+      {
+        todo: "Go to work",
+        isCompleted: false
+      }
+    ])
+
+
+    const Form = (props) => {
+
+      const [fillForm, setFillForm] = useState("")
+
+      const submitHandler = (e) => {
+        e.preventDefault()
+        setTodos([...todos, {todo: fillForm, isCompleted: false}])
+      }
+
+      return (
+        <div>
+          <form onSubmit={submitHandler}>
+            <input value={fillForm} onChange={e => setFillForm(e.target.value)}/>
+            <button>Add Todo</button>
+          </form>
+        </div>
+      )
+    }
+
+
+    // DELETE TODO
+  const deleteTodo = (todoIndex) => {
+
+
+    const filteredTodos = todos.filter((eachTodo, index) => {
+      if (index === todoIndex) {
+        return false
+      } else {
+        return true
+      }
+    })
+
+    // set new state
+    setTodos(filteredTodos)
+  }
+
+  // update after a completed is checked
+  const updateTodo = (todoIndex) => {
+ 
+    const copyTodos = [...todos]
+
+    if(copyTodos[todoIndex].isCompleted === true) {
+      copyTodos[todoIndex].isCompleted = false
+    } else {
+      copyTodos[todoIndex].isCompleted = true
+    }
+    setTodos(copyTodos)
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <Form/>
+      {
+        todos.map((eachTodo, todoIndex) => {
+          return (
+          <div key={todoIndex}>
+            <h3>Todo:</h3>{eachTodo.todo}
+            <input type="checkbox" checked={eachTodo.isCompleted} onChange={() => updateTodo(todoIndex)}/> <br />
+            <button onClick={() => deleteTodo(todoIndex)}>Delete</button>
+          </div>
+          )
+        })
+      }
     </div>
   );
 }
